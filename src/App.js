d81b6f2502f41e961c1cc13b35e76d1a4a1786cb
jrtaboraman <jr.taboraman@gmail.com>
2022-07-20 03:22:16 +0800

    Added contact page and integrated emailjs

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
 import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -11,8 +12,8 @@ function App() {
       <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
           <Route path="about" element={<About />} />
-          {/* <Route path="/contact" element={<Contact />} />
-          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+          {/* <Route path="/portfolio" element={<Portfolio />} />
           <Route path="/dashboard" element={<Dashboard />} /> */}
       </Route>
     </Routes>
